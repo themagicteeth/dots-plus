25ffbf2f90dde1145ff5253c48aa7254d6942671
w.s <w.s@openmailbox.org>
2017-09-04 19:41:33 -0500

    more firefox

--- .config/mozilla/local-settings.js
+++
@@ -1,7 +0,0 @@
-//
-// Must be symlinked to the firefox installation root directry. 
-// In my case:
-// ln -s ~/.config/mozilla/local-settings.js ~/.local/opt/firefox/defaults/pref/local-settings.js
-
-pref("general.config.obscure_value", 0);
-pref("general.config.filename", "mozilla.cfg");
--- .config/mozilla/user.js
+++
@@ -1,9 +0,0 @@
-security.mixed_content.block_display_content;true
-
-
-reader.parse-on-load.enabled
-
-extensions.pocket.oAuthConsumerKey;
-extensions.pocket.site;
-extensions.pocket.enabled;false
-extensions.pocket.api;
